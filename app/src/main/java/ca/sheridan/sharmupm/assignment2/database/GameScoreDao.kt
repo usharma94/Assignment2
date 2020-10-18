873a023ea517d213d57d2790634108ac170cd2ea
Upma <Upma.Sharma@ontario.ca>
2020-10-17 21:37:03 -0400

    Second Commit

--- app/src/main/java/ca/sheridan/sharmupm/assignment2/database/GameScoreDao.kt
+++ app/src/main/java/ca/sheridan/sharmupm/assignment2/database/GameScoreDao.kt
@@ -1,9 +1,11 @@
 package ca.sheridan.sharmupm.assignment2.database
 
 import androidx.lifecycle.LiveData
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 
+@Dao
 interface GameScoreDao {
 
     @Insert
